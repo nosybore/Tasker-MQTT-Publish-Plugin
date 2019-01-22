28503ac94fd171a6f6391bc7b0329ec3b43c5c4a
gitolicious <john@doe.com>
2019-01-22 20:33:27 +0100

    added option to skip SSL certificate verification

--- mqttpublishplugin/src/main/java/net/nosybore/mqttpublishplugin/BundleExtraKeys.java
+++ mqttpublishplugin/src/main/java/net/nosybore/mqttpublishplugin/BundleExtraKeys.java
@@ -1,11 +1,12 @@
 package net.nosybore.mqttpublishplugin;
 
 public class BundleExtraKeys {
-	
+
 	public static final String SERVER = "Server";
 	public static final String PORT = "Port";
 	public static final String CLIENT_ID = "ClientID";
 	public static final String SSL = "SSL";
+	public static final String SSL_IGNORE_CERT = "SSLIgnoreCert";
 	public static final String USERNAME = "Username";
 	public static final String PASSWORD = "Password";
 	public static final String TOPIC = "Topic";
